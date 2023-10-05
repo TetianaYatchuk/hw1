e551192773a56194bc4f15cc91bf8e0a3ff11238
TetianaYatchuk <tanya15115@gmail.com>
2023-10-05 13:06:43 +0300

    add user greetings

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let greeting = 'Hello';
+let sign = '!';
+
+let union = greeting + sign;
+
+alert(union);
+
+let question = prompt('What is your name?');
+
+alert(question);
+
+let question2 = prompt('Where are you from?');
+
+alert(question2);
+
+let text = 'Nice to meet you';
+let sign2 = ',';
+let sign3 = '!';
+let text2 = 'You are from';
+let sign4 = '!';
+
+let union2 = text + sign2 + ' ' + question + sign3 + text2 + ' ' + question2 + sign4;
+
+alert(union2);
